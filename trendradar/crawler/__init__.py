aa2cbead40298769b25d65b376dc91d0a371d67f
yuyu <36332380+yuhao0406@users.noreply.github.com>
2026-07-10 14:09:23 +0800

    Update __init__.py

--- trendradar/crawler/__init__.py
+++ trendradar/crawler/__init__.py
@@ -1,8 +1,17 @@
 # coding=utf-8
 """
 爬虫模块 - 数据抓取功能
+
+支持:
+- NewsNow API
+- HTML Source
 """
 
 from trendradar.crawler.fetcher import DataFetcher
+from trendradar.crawler.collector import Collector
 
-__all__ = ["DataFetcher"]
+
+__all__ = [
+    "DataFetcher",
+    "Collector",
+]
